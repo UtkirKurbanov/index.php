a8270aa3e4d1623d35744633328d5559d34ac48a
Utkir <karshi2700@mail.ru>
2022-04-20 11:34:35 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $svetafor = 'red';
+    if($svetafor == 'green') {
+        echo 'Светафор зеленый, я перехожу дорогу';
+    }   else if($svetafor == 'yellow') {
+            echo 'Сейчас светафор желтый';
+    }    
+    else {
+        echo 'Жду когда загорится зеленый';
+    }
+
+?>
